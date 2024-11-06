176053cea7081f030827c28d6a5dbf2d92e99951
Nitrix <126798153+nitrixly@users.noreply.github.com>
2024-11-06 12:29:14 +0530

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,1 +1,26 @@
+import discord
+import os
+from discord.ext import commands
+from settings.config import TOKEN, OWNER, PREFIX
 
+class Bot(commands.Shard):
+    def __init__(self):
+        super().__init__(command_prefix=PREFIX, intents=discord.Intents.all(), shard_count=1)
+        self.owner_ids = OWNER
+
+    async def on_ready(self):
+        print(f"{self.user.name} is ready!")
+
+    async def setup_hook(self):
+        await self.load_extension('jishaku')
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                try:
+                    await self.load_extension(f'cogs.{filename[:-3]}')
+                    print(f'[Loaded] `{filename}`')
+                except Exception as e:
+                    print(f'Failed to load {filename}: {e}')
+        await self.tree.sync()
+
+client = Bot()
+client.run(TOKEN)
